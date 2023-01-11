0464e3dc73759cf8ec8f1e1d78c53d6281c36894
Marek Pecha <marek.pecha@vsb.cz>
2023-01-11 10:42:17 +0100

    Process labels (ESA CCI)

--- procs/ts.py
+++ procs/ts.py
@@ -2,7 +2,6 @@ import os.path
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from enum import Enum
 
@@ -23,35 +22,47 @@ class TimeSeries(object):
                  path_geotiff: str = None,
                  path_labels: str = None):
 
-        self._impath = None
+        self._ds_sources = None
+        self._ds_labels = None
+
+        self._path_img = None
         if path_geotiff is not None:
-            self.path_geotiff = path_geotiff
+            self.path_img = path_geotiff
 
         self._label_path = None
         if path_labels is not None:
             self.path_labels = path_labels
 
-        self._ds = None
+        # dictionary contains time series
         self._dict_ts = None
+        self._df_labels = None
 
-        self._df_dates = None
+        # dates pandas data frames
+        self._df_dates_sources = None
+        self._df_dates_labels = None
 
     def __len__(self) -> int:
 
         return len(self._dict_ts) if len(self._dict_ts) is not None else 0
 
     @property
-    def path_geotiff(self) -> str:
+    def path_img(self) -> str:
 
-        return self._impath
+        return self._path_img
 
-    @path_geotiff.setter
-    def path_geotiff(self, fn: str) -> None:
+    @path_img.setter
+    def path_img(self, fn: str) -> None:
 
         if not os.path.exists(fn):
             raise IOError('File {} does not exist!'.format(fn))
 
-        self._impath = os.path.abspath(fn)
+        path_source = os.path.abspath(fn)
+
+        if self._path_img == path_source:
+            return
+
+        del self._ds_sources; self._ds_sources = None
+        self._path_img = path_source
 
     @property
     def path_labels(self) -> str:
@@ -66,53 +77,97 @@ class TimeSeries(object):
 
         path_source = os.path.abspath(fn)
 
-        if self._label_path != path_source:
-            self._label_path = path_source
+        if self._label_path == path_source:
+            return
+
+        del self._ds_labels; self._ds_labels = None
+        self._label_path = path_source
 
     @property
     def nrasters(self) -> int:
 
-        if self._ds is None:
+        if self._ds_sources is None:
             try:
-                self.__loadGeoTiff()
+                self.__loadGeoTiff_SOURCES()
             except IOError:
-                print('Can not load geotiff {}'.format(self.path_geotiff))
+                print('Can not load geotiff {}'.format(self.path_img))
+                return 0
 
-        return self._ds.RasterCount
+        return self._ds_sources.RasterCount
 
     @property
-    def npixels(self) -> int:
+    def raster_npixels(self) -> int:
 
-        if self._ds is None:
+        if self._ds_sources is None:
             try:
-                self.__loadGeoTiff()
+                self.__loadGeoTiff_SOURCES()
             except IOError:
-                print('Can not load geotiff {}'.format(self.path_geotiff))
+                print('Can not load geotiff {}'.format(self.path_img))
 
-        w, h = self._ds.RasterXSize, self._ds.RasterYSize
+        w, h = self._ds_sources.RasterXSize, self._ds_sources.RasterYSize
         return w * h
+
+    @property
+    def raster_shape(self) -> tuple:
+
+        if self._ds_sources is None:
+            try:
+                self.__loadGeoTiff_SOURCES()
+            except IOError:
+                print('Can not load geotiff {}'.format(self.path_img))
+
+        shape = (self._ds_sources.RasterXSize, self._ds_sources.RasterYSize)
+        return shape
+
+    @property
+    def nlabels(self) -> int:
+
+        if self._ds_labels is None:
+            try:
+                self.__loadGeoTIff_LABELS()
+            except IOError:
+                print('Can not load geotiff {}'.format(self.path_labels))
+
+        return self._ds_labels.RasterCount
 
     def __reset(self) -> None:
 
-        del self._ds; self._ds = None
+        del self._ds_sources; self._ds_sources = None
         del self._dict_ts; self._dict_ts = None
-        del self._df_dates; self._df_dates = None
+        del self._df_dates_sources; self._df_dates_sources = None
 
-    def __loadGeoTiff(self) -> None:
+    def __loadGeoTiff_SOURCES(self) -> None:
 
-        if self._ds is not None:
+        if self._ds_sources is not None:
             self.__reset()
 
-        if self.path_geotiff is None:
-            raise IOError('GeoTIFF file is not set!')
+        if self.path_img is None:
+            raise IOError('GeoTIFF file (source) is not set!')
 
-        self._ds = gdal.Open(self.path_geotiff)
+        self._ds_sources = gdal.Open(self.path_img)
 
-    def __determineListDates(self) -> None:
+    def __loadGeoTIff_LABELS(self) -> None:
+
+        if self._ds_labels is not None:
+            self.__reset()
+
+        if self.path_labels is None:
+            raise IOError('GeoTIFF file (labels) is not set!')
+
+        self._ds_labels = gdal.Open(self.path_labels)
+
+    def __determineListDates_SOURCES(self) -> None:
+
+        if self._ds_sources is None:
+            try:
+                self.__loadGeoTiff_SOURCES()
+            except IOError:
+                print('Can not load geotiff {}'.format(self.path_img))
+                return
 
         lst = []
         for id_raster in range(self.nrasters):
-            rs_band = self._ds.GetRasterBand(id_raster + 1)
+            rs_band = self._ds_sources.GetRasterBand(id_raster + 1)
             dsc_band = rs_band.GetDescription()
 
             band_date = band2date(dsc_band)
@@ -127,62 +182,93 @@ class TimeSeries(object):
         df_dates['begin'] = raster_from
         df_dates['end'] = raster_to
 
-        self._df_dates = df_dates
+        self._df_dates_sources = df_dates
+
+    def __determineListDates_LABELS(self) -> None:
+
+        if self._ds_labels is None:
+            try:
+                self.__loadGeoTIff_LABELS()
+            except IOError:
+                print('Can not load geotiff {}'.format(self.path_labels))
+                return
+
+        lst = []
+        for id_raster in range(self.nlabels):
+            rs_band = self._ds_labels.GetRasterBand(id_raster + 1)
+            dsc_band = rs_band.GetDescription()
+
+            band_date = band2date(dsc_band)
+            lst.append(band_date)
+
+        df_dates = pd.DataFrame(lst)
+        self._df_dates_labels = df_dates
 
     @property
-    def list_dates(self):
+    def list_dates_imgs(self):
 
-        if self._df_dates is not None:
-            return self._df_dates
+        if self._df_dates_sources is not None:
+            return self._df_dates_sources
 
-        if self._ds is None:
-            try:
-                self.__loadGeoTiff()
-            except IOError:
-                print('Can not load geotiff {}'.format(self.path_geotiff))
+        self.__determineListDates_SOURCES()
+        return self._df_dates_sources
 
-        self.__determineListDates()
+    @property
+    def list_dates_labels(self):
 
-        return self._df_dates
+        if self._df_dates_labels is not None:
+            return self._df_dates_labels
+
+        self.__determineListDates_LABELS()
+        return self._df_dates_labels
 
     def get_imraster(self, id_raster) -> np.ndarray:
 
-        if self._ds is None:
-            self.__loadGeoTiff()
+        if self._ds_sources is None:
+            self.__loadGeoTiff_SOURCES()
 
         # [red, near infra-red (nir), blue, green, short-wave infra-red (swir) 1, swir2, swir3]
-        rs_band = self._ds.GetRasterBand(id_raster + 1).ReadAsArray()
+        rs_band = self._ds_sources.GetRasterBand(id_raster + 1).ReadAsArray()
         return rs_band
 
-    def get_pandas(self, id_pixel) -> pd.DataFrame:
+    def get_ts(self, id_pixel) -> pd.DataFrame:
 
         if self._dict_ts is None:
-            self.load()
+            self.__create_ts_dict()
 
-        df = pd.DataFrame.from_dict(self.get_dict(id_pixel))
+        df = pd.DataFrame.from_dict(self.__get_dict_ts(id_pixel))
         return df
 
-    def get_dict(self, index) -> dict:
+    def get_labels(self, id_time) -> pd.DataFrame:
 
-        if self._ds is None:
-            self.load()
+        if self._df_labels is None:
+            self.__create_labels_dataframe()
+
+        return self._df_labels[id_time]
+
+    def __get_dict_ts(self, index) -> dict:
+
+        if self._dict_ts is None:
+            self.__create_ts_dict()
 
         return self._dict_ts[index]
 
-    def load(self) -> None:
+    def __create_ts_dict(self) -> None:
 
-        if self._ds is None:
+        if self._ds_sources is None:
             try:
-                self.__loadGeoTiff()
+                self.__loadGeoTiff_SOURCES()
             except IOError:
-                print('Can not load geotiff {}'.format(self.path_geotiff))
+                print('Can not load geotiff (sources) {}'.format(self.path_img))
 
-        npixels = self.npixels
+        # sources
+
+        npixels = self.raster_npixels
         dict_ts = {px_id: {'date': []} for px_id in range(npixels)}
 
         for i in range(self.nrasters):
 
-            rs_band = self._ds.GetRasterBand(i + 1)
+            rs_band = self._ds_sources.GetRasterBand(i + 1)
             dsc_band = rs_band.GetDescription()
 
             band_date = band2date(dsc_band)
@@ -201,7 +287,36 @@ class TimeSeries(object):
 
                 dict_ts[px_id][band_name].append(val)
 
-        self._dict_ts = dict_ts
+        del self._dict_ts; self._dict_ts = dict_ts
+
+    def __create_labels_dataframe(self) -> None:
+
+        if self._ds_labels is None:
+            try:
+                self.__determineListDates_LABELS()
+            except IOError:
+                print('Ce not load geotiff (labels) {}'.format(self.path_labels))
+
+        dict_labels = {}
+        for i in range(self.nlabels):
+            rs_band = self._ds_labels.GetRasterBand(i + 1)
+            dsc_band = rs_band.GetDescription()
+
+            band_date = band2date(dsc_band)
+            band_date = pd.Timestamp(band_date, unit='D')
+
+            np_band = rs_band.ReadAsArray().flatten()
+            dict_labels[band_date] = np_band
+
+        del self._df_labels; self._df_labels = pd.DataFrame.from_dict(dict_labels)
+
+    def load(self) -> None:
+
+        if self._dict_ts is None:
+            self.__create_ts_dict()
+
+        if self._df_labels is None:
+            self.__create_labels_dataframe()
 
 
 # tests
@@ -210,21 +325,29 @@ if __name__ == '__main__':
     from PIL import Image, ImageEnhance
 
     im_path = 'tutorials/ak_2004_500px2_epsg3338_area_0.tif'
+    label_path = 'tutorials/ak_2004_500px2_epsg3338_area_0_labels.tif'
 
-    ts = TimeSeries(path_geotiff=im_path)
-    with elapsed_timer('Get date list'):
-        print(ts.list_dates)
+    ts = TimeSeries(path_geotiff=im_path, path_labels=label_path)
+    with elapsed_timer('Get date list (imgs)'):
+        print(ts.list_dates_imgs)
 
-    with elapsed_timer('Get pandas serie px=0'):
-        ts_px0 = ts.get_pandas(0)
+    with elapsed_timer('Get date list (labels)'):
+        print(ts.list_dates_labels)
 
-    x = ts_px0['date']
-    y = ts_px0['b01']
+    with elapsed_timer('Get labels pandas'):
+        print(ts.get_labels(pd.Timestamp('2004-01')))
 
-    plt.plot(x, y)
-    plt.grid()
-    plt.xticks(rotation=15)
-    plt.show()
+    # ts.__loadLabels()
+    # with elapsed_timer('Get pandas serie px=0'):
+    #     ts_px0 = ts.get_pandas(0)
+    #
+    # x = ts_px0['date']
+    # y = ts_px0['b01']
+    #
+    # plt.plot(x, y)
+    # plt.grid()
+    # plt.xticks(rotation=15)
+    # plt.show()
 
     # ts = TimeSeries(path_geotiff=im_path)
     #
