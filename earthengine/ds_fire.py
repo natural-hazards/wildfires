77ebe0549ea2b3eba0813f7e31825adf2b37b5a7
Marek Pecha <marek.pecha@vsb.cz>
2023-01-11 12:36:50 +0100

    Implementations related to loading MTBS collections

--- earthengine/ds_fire.py
+++ earthengine/ds_fire.py
@@ -15,7 +15,7 @@ class EarthEngineFireDatasets(object):
 
             def filter_confidence(value: float):
                 def fn_filter(image):
-                    mask = image.select('ConfidenceLevel').gt(value)
+                    mask = image.select('ConfidenceLevel').gte(value)
                     return image.updateMask(mask)
 
                 return fn_filter
@@ -28,5 +28,30 @@ class EarthEngineFireDatasets(object):
                 ds_fire_cci = ds_fire_cci.map(filter_confidence(confidence_level))
             ds_fire_cci = ds_fire_cci.select('ConfidenceLevel')
             burn_area = ds_fire_cci.max()
+
+            return burn_area
+
+    class FireMTBS:
+
+        @staticmethod
+        def getBurnArea(severity_from, severity_to, start_date='2001-01-01', end_date='2021-01-01'):
+
+            def filter_severity(value_from: float, value_to: float):
+                def fn_filter(image):
+                    #
+                    mask = image.select('Severity').gte(value_from)
+                    mask = mask.lte(value_to)
+                    return image.updateMask(mask)
+
+                return fn_filter
+
+            ds_fire_mtbs = earthengine.ImageCollection('USFS/GTAC/MTBS/annual_burn_severity_mosaics/v1')
+            date_filer = earthengine.Filter.date(start_date, end_date)
+
+            ds_fire_mtbs = ds_fire_mtbs.filter(date_filer)
+            if severity_from > 0:
+                ds_fire_mtbs = ds_fire_mtbs.map(filter_severity(severity_from, severity_to))
+            ds_fire_mtbs = ds_fire_mtbs.select('Severity')
+            burn_area = ds_fire_mtbs.max()
 
             return burn_area
