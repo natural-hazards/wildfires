d9cabd0015ebb98196d5a63421b3487fd18b1840
Marek Pecha <marek.pecha@vsb.cz>
2023-06-08 11:43:02 -0500

    IO: load source

--- mlfire/models/permonsvm/io.py
+++ mlfire/models/permonsvm/io.py
@@ -1,13 +1,21 @@
 
 from mlfire.utils.functool import lazy_import
 
+_io = lazy_import('os')
+
 _io_utils = lazy_import('mlfire.utils.io')
 _np = lazy_import('numpy')
 
 
-def loadResult(fn_pred: str, fn_known: str) -> _np.ndarray:
+def loadResult(fn_pred: str, fn_mask: str) -> _np.ndarray:
 
-    mask = _io_utils.loadArrayHDF5(fn_known, 'mask')
+    if not _io.path.exists(fn_pred):
+        raise IOError('File {} not exist!'.format(fn_pred))
+
+    if not _io.path.exists(fn_mask):
+        raise IOError('File {} not exist!'.format(fn_mask))
+
+    mask = _io_utils.loadArrayHDF5(fn_mask, 'mask')
     pred = _io_utils.loadArrayHDF5(fn_pred, 'y_predictions')
 
     y_pred = _np.empty(shape=mask.shape, dtype=pred.dtype); y_pred[:] = _np.nan
@@ -16,10 +24,27 @@ def loadResult(fn_pred: str, fn_known: str) -> _np.ndarray:
     return y_pred
 
 
+def loadSource(fn: str) -> _np.ndarray:
+
+    if not _io.path.exists(fn):
+        raise IOError('File {} not exist!'.format(fn))
+
+    mask = _io_utils.loadArrayHDF5(fn, 'mask')
+    labels = _io_utils.loadArrayHDF5(fn, 'y')
+
+    y = _np.empty(shape=mask.shape, dtype=labels.dtype); y[:] = _np.nan
+    y[~mask] = labels
+
+    return y
+
+
 if __name__ == '__main__':
 
     fn_test = '../../../data/h5/mtbs/ak_modis_2004_2005_100km_test.h5'
     fn_pred = '../../../output/permonsvm/ak_modis_2004_2005_100km_test_predictions.h5'
 
-    y_pred = loadResult(fn_pred=fn_pred, fn_known=fn_test)
+    y_test = loadSource(fn=fn_test)
+    y_pred = loadResult(fn_pred=fn_pred, fn_mask=fn_test)
+
+    print(y_test.shape)
     print(y_pred.shape)
