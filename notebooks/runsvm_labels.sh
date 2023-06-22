d988b07121d7e22d9babb8c87352dc939884ad4f
Marek Pecha <marek.pecha@vsb.cz>
2023-06-22 14:13:13 +0200

    Predict probability using PermonSVM

---
+++ notebooks/runsvm_labels.sh
@@ -0,0 +1,29 @@
+PERMON_SVM_DIR=$1
+PETSC_ARCH=$2
+TOOLS_DIR=$3
+
+echo $PERMON_SVM_DIR
+
+DATA_DIR=${TOOLS_DIR}/data/h5/mtbs
+OUTPUT_DIR=${TOOLS_DIR}/output/permonsvm
+
+DS_PREFIX=ak_modis_2004_2005_100km
+
+FN_TRAIN=${DATA_DIR}/${DS_PREFIX}_labels_training.h5
+FN_TEST=${DATA_DIR}/${DS_PREFIX}_labels_test.h5
+
+FN_TRAIN_PREDICTS=${OUTPUT_DIR}/${DS_PREFIX}_training_labels_predictions.h5
+FN_TEST_PREDICTS=${OUTPUT_DIR}/${DS_PREFIX}_test_labels_predictions.h5
+
+LOSS_TYPE=L1
+MAT_TYPE=dense
+
+${PERMON_SVM_DIR}/${PETSC_ARCH}/bin/permonsvmfile \
+-fllop_trace \
+-f_training ${FN_TRAIN} -Xt_training_mat_type ${MAT_TYPE} \
+-f_test ${FN_TEST} -Xt_test_mat_type ${MAT_TYPE} \
+-svm_view_io \
+-qps_mpgp_expansion_type projcg -qps_mpgp_gamma 10 -qps_view_convergence \
+-svm_loss_type ${LOSS_TYPE} -svm_binary_mod 2 -svm_C 0.01 \
+-svm_view_report \
+-f_training_predictions ${FN_TRAIN_PREDICTS} -f_test_predictions ${FN_TEST_PREDICTS}
--- notebooks/runsvm_probability.sh
+++ notebooks/runsvm_probability.sh
@@ -6,12 +6,12 @@ OUTPUT_DIR=${TOOLS_DIR}/output/permonsvm
 
 DS_PREFIX=ak_modis_2004_2005_100km
 
-FN_TRAIN=${DATA_DIR}/${DS_PREFIX}_training.h5
-FN_CALIB=${DATA_DIR}/${DS_PREFIX}_calib.h5
-FN_TEST=${DATA_DIR}/${DS_PREFIX}_test.h5
+FN_TRAIN=${DATA_DIR}/${DS_PREFIX}_probability_training.h5
+FN_CALIB=${DATA_DIR}/${DS_PREFIX}_probability_calib.h5
+FN_TEST=${DATA_DIR}/${DS_PREFIX}_probability_test.h5
 
-FN_TRAIN_PREDICTS=${OUTPUT_DIR}/${DS_PREFIX}_training_predictions.h5
-FN_TEST_PREDICTS=${OUTPUT_DIR}/${DS_PREFIX}_test_predictions.h5
+FN_TRAIN_PREDICTS=${OUTPUT_DIR}/${DS_PREFIX}_training_probability_predictions.h5
+FN_TEST_PREDICTS=${OUTPUT_DIR}/${DS_PREFIX}_test_probability_predictions.h5
 
 LOSS_TYPE=L1
 MAT_TYPE=dense
@@ -19,10 +19,11 @@ MAT_TYPE=dense
 ${PERMON_SVM_DIR}/src/tutorials/ex5 \
 -fllop_trace \
 -f_training ${FN_TRAIN} -Xt_training_mat_type ${MAT_TYPE} -f_calib ${FN_CALIB} -Xt_calib_mat_type ${MAT_TYPE} \
--f_test ${FN_TEST} -Xt_test_mat_type ${MAT_TYPE} -svm_view_io \
--uncalibrated_qps_mpgp_expansion_type projcg -uncalibrated_qps_mpgp_gamma 10 \
+-f_test ${FN_TEST} -Xt_test_mat_type ${MAT_TYPE} \
+-svm_view_io \
+-uncalibrated_qps_mpgp_expansion_type projcg -uncalibrated_qps_mpgp_gamma 10 -uncalibrated_qps_view_convergence \
 -uncalibrated_svm_loss_type ${LOSS_TYPE} -uncalibrated_svm_binary_mod 2 -uncalibrated_svm_C 0.01 \
 -svm_threshold 0.50 -tao_view \
 -svm_view_report \
--f_training_predictions ${FN_TRAIN_PREDICTS} -f_test_predictions ${FN_TEST_PREDICTS} -uncalibrated_qps_view_convergence
+-f_training_predictions ${FN_TRAIN_PREDICTS} -f_test_predictions ${FN_TEST_PREDICTS}
 
